fc0a5dd9b76cacd6959e3a100c828139f1345b72
Weilin Zhou <98453985+Rockaaaaa@users.noreply.github.com>
2022-10-23 05:52:15 -0700

    pickup

---
+++ Assets/Scripts/PlayerPickup.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour
+{
+    [SerializeField] private float pickupRange;
+    [SerializeField] private LayerMask pickupLayer;
+
+    private Camera cam;
+    private Inventory inventory;
+
+    void Start()
+    {
+        GetReferences();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, pickupRange, pickupLayer))
+            {
+                Weapon newItem = hit.transform.GetComponent<ItemObject>().item as Weapon;
+                inventory.AddItem(newItem);
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+
+    private void GetReferences()
+    {
+        cam = GetComponentInChildren<Camera>();
+        inventory = GetComponent<Inventory>();
+    }
+}
